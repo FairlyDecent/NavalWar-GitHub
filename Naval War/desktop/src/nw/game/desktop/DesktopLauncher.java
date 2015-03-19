5414d0da6f98ce34e3c4f92578d018a0c459c179
Félix Poitras <felixpoitras@videotron.ca>
2015-03-19 10:15:28 -0400

    Created game project

---
+++ Naval War/desktop/src/nw/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nw.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nw.game.NavalWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NavalWar(), config);
+	}
+}
